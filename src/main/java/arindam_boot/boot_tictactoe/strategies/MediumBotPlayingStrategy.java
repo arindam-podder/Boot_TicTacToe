3352ab04a4703746508650591b8e51668d7a0f79
Arindam Podder <arindam.podder@sevyamm.com>
2024-03-25 18:02:02 +0530

    TTT code with bot

---
+++ src/main/java/arindam_boot/boot_tictactoe/strategies/MediumBotPlayingStrategy.java
@@ -0,0 +1,12 @@
+package arindam_boot.boot_tictactoe.strategies;
+
+import arindam_boot.boot_tictactoe.model.Board;
+import arindam_boot.boot_tictactoe.model.Move;
+
+public class MediumBotPlayingStrategy implements BotPlayingStrategy {
+
+    @Override
+    public Move makeMove(Board board) {
+        return null;
+    }
+}
